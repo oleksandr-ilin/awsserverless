59f39f2d845590388dec5c3bafcd1a52b52d3e60
Alex ILIN <oleksandr_ilin@epam.com>
2024-09-10 07:08:36 -0700

    task04 more policies to create sqs

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.syndicate.deployment.annotations.events.SqsEvents;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
@@ -27,7 +28,7 @@ import java.util.Map;
         runtime = DeploymentRuntime.JAVA11,
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 100)
+@SqsEvents({@SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 10)})
 @DependsOn(
         name = "async_queue",
         resourceType = ResourceType.SQS_QUEUE
